7a4c62c6f3fac9bf52f7c4bf8936fbff840f915a
Carlosayol <60300191+Carlosayol@users.noreply.github.com>
2020-11-07 16:51:08 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function change() // no ';' here
+{
+    var elem = document.getElementById("myButton1");
+    if (elem.value=="Efecita Broder") elem.value = "Alonso Padre";
+    else elem.value = "Efecita Broder";
+}
