869d070047bc408968a9a5a3f4635b4ead07d72c
Carlosayol <60300191+Carlosayol@users.noreply.github.com>
2020-11-13 14:20:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ function change()
 {
     var elem = document.getElementById("imageID");
     if (elem.getAttribute("src") =="feliznavidad1.png") elem.setAttribute("src", "feliznavidad2.jpg" )
-    else elem.setAttribute("src","feliznavidad1.png" ) ;
+    //else elem.setAttribute("src","feliznavidad1.png" ) ;
+    else elem.setAibute("src","feliznavidad1.png" ) ;
 }
