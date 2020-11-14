868bfa996d00963e5494d80e8e224aaeadd63de1
Carlosayol <carlosayol@unisabana.edu.co>
2020-11-14 08:35:02 -0500

    feliz navidad

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function change()
 {
     var elem = document.getElementById("imageID");
-    if (elem.getAttribute("src") =="feliznavidad1.png") elem.setAttribute("src", "https://thumbs.dreamstime.com/z/feliz-navidad-frase-de-la-caligraf%C3%ADa-del-d%C3%ADa-de-fiesta-de-la-navidad-de-las-letras-y-del-a%C3%B1o-nuevo-en-el-espa%C3%B1ol-aislado-en-el-79837104.jpg" )
+    if (elem.getAttribute("src") ="feliznavidad1.png") elem.setAttribute("src", "feliznavidad3.jpg" )
     else elem.setAttribute("src","feliznavidad1.png" ) ;
 }
